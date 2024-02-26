c1dee790443afb7f93574d0f69dcc2e4557a69b4
qal <lequyetanh@outlook.com>
2024-02-27 05:10:58 +0700

    update data

--- DataDB/attach_db.sh
+++ DataDB/attach_db.sh
@@ -1,3 +1,3 @@
 sleep 15s
 /opt/mssql-tools/bin/sqlcmd -S . -U sa -P Password1$ \
--Q "CREATE DATABASE [ArtHubDb2024] ON (FILENAME ='/var/opt/mssql/data/ArtHubDb2024.mdf') LOG ON (FILENAME = '/var/opt/mssql/data/ArtHubDb2024.ldf') FOR ATTACH;"
+-Q "CREATE DATABASE [ArtHubDb2024] ON (FILENAME ='/var/opt/mssql/data/ArtHubDb2024.mdf') LOG ON (FILENAME = '/var/opt/mssql/data/ArtHubDb2024_log.ldf') FOR ATTACH;"
