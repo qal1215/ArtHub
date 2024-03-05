7b39781a254df20a19f6c536112c8542eca07967
qal <lequyetanh@outlook.com>
2024-03-06 04:46:14 +0700

    update IBalanceService

--- ArtHub.Service/Contracts/IBalanceService.cs
+++ ArtHub.Service/Contracts/IBalanceService.cs
@@ -1,6 +1,17 @@
-﻿namespace ArtHub.Service.Contracts
+﻿using ArtHub.BusinessObject;
+
+namespace ArtHub.Service.Contracts
 {
     public interface IBalanceService
     {
+        Task<List<HistoryTransaction>> GetHistoryTransactionsByAccountId(int accountId);
+
+        Task<List<HistoryTransaction>> GetHistoryTransactionsByArtworkId(int artworkId);
+
+        Task<HistoryTransaction> DepositBalanceAsync(int accountId, decimal amount);
+
+        Task<HistoryTransaction> WithdrawBalanceAsync(int accountId, decimal amount);
+
+        Task<HistoryTransaction> PurchaseArtworkAsync(int accountId, int artworkId, decimal amount);
     }
 }
